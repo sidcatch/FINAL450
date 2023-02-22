a0c0ad687393016403709af500c36eccbc1b1eb3
Syed Imauddin <syedprogrammer@yahoo.com>
2023-02-22 16:57:17 +0530

    Find the next greater element of all the elements in an array

---
+++ stack-queue/next-greater-circular.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+    int a[] = {3, 5, 6, 35, 2, 4, 56, 75, 3, 3, 60, 23};
+
+    int n = sizeof(a) / sizeof(a[0]);
+    int r[n];
+
+    stack<int> s;
+
+    for (int i = 2 * n - 1; i >= 0; i--)
+    {
+
+        while (!s.empty() && s.top() <= a[i % n])
+        {
+            s.pop();
+        }
+
+        if (s.empty())
+            r[i % n] = -1;
+        else
+            r[i % n] = s.top();
+
+        s.push(a[i % n]);
+    }
+
+    for (int i = 0; i < n; i++)
+        cout << r[i] << " ";
+    cout << endl;
+
+    return 0;
+}
