b91456a75a24fa6b9b21fcdfec5497e12761347b
Syed Imauddin <syedprogrammer@yahoo.com>
2023-04-08 22:04:18 +0530

    refactor

--- array/min-height-difference.cpp
+++ array/min-height-difference.cpp
@@ -5,18 +5,19 @@ int getMinDiff(int arr[], int n, int k)
 {
     sort(arr, arr + n);
 
-    int tempmin = arr[0];
-    int tempmax = arr[n - 1];
-    int ans = tempmax - tempmin;
+    int tempmin = arr[0] + k;
+    int tempmax = arr[n - 1] - k;
+    int ans = arr[n - 1] - arr[0];
+    // int ans = INT_MAX;
 
     for (int i = 1; i < n; i++)
     {
         if (arr[i] - k < 0)
             continue;
 
-        tempmin = min(arr[0] + k, arr[i] - k);
+        tempmin = min(tempmin, arr[i] - k);
 
-        tempmax = max(arr[i - 1] + k, arr[n - 1] - k);
+        tempmax = max(tempmax, arr[i - 1] + k);
 
         ans = min(ans, tempmax - tempmin);
     }
