606f8aaf6a1a252cd83a833dbadf559dc9d8d84e
Syed Imauddin <syedprogrammer@yahoo.com>
2023-08-01 13:28:33 +0530

    Max profit by buying and selling stock - recursive approach

---
+++ array/buy-and-sell-stock2.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int findMaximumProfit(vector<int> &prices, int i, bool buy, vector<vector<int>> &v)
+{
+
+    if (i < 0)
+        return 0;
+
+    if (i == 0 && buy)
+        return v[i][buy] = -prices[0];
+    if (i == 0 && !buy)
+        return v[i][buy] = 0;
+
+    if (v[i][buy] != -1)
+        return v[i][buy];
+
+    if (buy)
+        return v[i][buy] = max(-prices[i],
+                               findMaximumProfit(prices, i - 1, buy, v));
+
+    else
+        return v[i][buy] = max(prices[i] + findMaximumProfit(
+                                               prices, i - 1, !buy, v),
+                               findMaximumProfit(prices, i - 1, buy, v));
+}
+
+int maxProfit(vector<int> &prices)
+{
+
+    int n = prices.size();
+    vector<vector<int>> v(n, vector<int>(2, -1));
+
+    return findMaximumProfit(prices, n - 1, 0, v);
+}
+
+int main()
+{
+    vector<int> prices = {7, 1, 5, 3, 6, 4};
+
+    int ans = maxProfit(prices);
+
+    cout << ans << endl;
+
+    return 0;
+}
---
+++ array/buy-and-sell-stock3.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int findMaximumProfit(vector<int> &prices, int i, bool buy, vector<vector<int>> &v)
+{
+
+    if (i >= prices.size())
+        return 0;
+
+    if (v[i][buy] != -1)
+        return v[i][buy];
+
+    if (buy)
+        return v[i][buy] = max(-prices[i] + findMaximumProfit(prices, i + 1, !buy, v),
+                               findMaximumProfit(prices, i + 1, buy, v));
+
+    else
+        return v[i][buy] = max(prices[i],
+                               findMaximumProfit(prices, i + 1, buy, v));
+}
+
+int maxProfit(vector<int> &prices)
+{
+
+    int n = prices.size();
+    vector<vector<int>> v(n, vector<int>(2, -1));
+
+    return findMaximumProfit(prices, 0, 1, v);
+}
+
+int main()
+{
+
+    vector<int> prices = {7, 1, 5, 3, 6, 4};
+
+    int ans = maxProfit(prices);
+
+    cout << ans << endl;
+
+    return 0;
+}
