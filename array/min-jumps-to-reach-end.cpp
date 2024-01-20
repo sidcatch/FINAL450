712646061dbc6d25663adabf8a3b4e26b8763c0b
Syed Imauddin <syedprogrammer@yahoo.com>
2024-01-20 23:27:50 +0530

    Minimum jumps to reach end of an array

---
+++ array/min-jumps-to-reach-end.cpp
@@ -0,0 +1,52 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int minJumps(int arr[], int n)
+{
+
+    if (n <= 1)
+        return 0;
+    if (arr[0] >= n - 1)
+        return 1;
+    if (arr[0] == 0)
+        return -1;
+
+    int currMaxReach = arr[0];
+    int stepsLeftInCurrJump = arr[0];
+    int currJump = 1;
+
+    int i = 1;
+    for (i = 1; i < n; i++)
+    {
+
+        if (i == n - 1)
+            return currJump;
+        if (arr[i] >= (n - 1) - i)
+            return currJump + 1;
+
+        currMaxReach = max(currMaxReach, i + arr[i]);
+        stepsLeftInCurrJump--;
+
+        if (stepsLeftInCurrJump == 0)
+        {
+            currJump++;
+            if (i >= currMaxReach)
+                return -1;
+            stepsLeftInCurrJump = currMaxReach - i;
+        }
+    }
+
+    return -1;
+}
+
+int main()
+{
+    int arr[] = {2, 3, 1, 1, 4};
+
+    int size = sizeof(arr) / sizeof(int);
+
+    cout << minJumps(arr, size) << endl;
+    return 0;
+}
