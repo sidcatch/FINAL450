cbf0e0f6f881f25fa50a3823ef57cc9fcb06d110
Syed Imauddin <syedprogrammer@yahoo.com>
2024-01-22 00:32:31 +0530

    fix

--- array/count-inversions.cpp
+++ array/count-inversions.cpp
@@ -11,7 +11,7 @@ int merge(int arr[], int temp[], int left, int middle, int right)
 
     while (i <= middle && j <= right)
     {
-        if (arr[i] < arr[j])
+        if (arr[i] <= arr[j])
             temp[k++] = arr[i++];
         else
         {
@@ -59,8 +59,8 @@ int mergesort(int arr[], int size)
 int main()
 {
 
-    int a[] = {2, 4, 1, 3, 5};
-    int size = 5;
+    int a[] = {2, 3, 4, 1, 3, 5};
+    int size = 6;
 
     for (int i = 0; i < size; i++)
         cout << a[i] << ", ";
