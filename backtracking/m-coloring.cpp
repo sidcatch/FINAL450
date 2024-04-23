f24a149c9c659e9d5d37026c5e42b632cad413df
Syed Imauddin <syedprogrammer@yahoo.com>
2024-04-23 11:05:56 +0530

    fix

--- backtracking/m-coloring.cpp
+++ backtracking/m-coloring.cpp
@@ -1,95 +1,69 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <unordered_map>
 using namespace std;
 
-const int N = 4;
-
-void printGrid(bool graph[N][N])
-{
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            cout << graph[i][j] << " ";
-        }
-        cout << endl;
-    }
-    cout << endl;
-}
-
-bool isGraphColored(int &n, unordered_map<int, int> &nodeColors)
+bool isSafe(int v, bool graph[101][101], int color[101], int c, int V)
 {
 
-    for (int i = 0; i < N; i++)
-        if (nodeColors.find(i) == nodeColors.end())
-        {
-            n = i;
+    for (int i = 0; i < V; i++)
+        if (graph[v][i] && c == color[i])
             return false;
-        }
 
     return true;
 }
 
-bool isValid(bool graph[N][N], int n, int m, unordered_map<int, int> &nodeColors)
+bool graphColoringUtil(bool graph[101][101], int m, int color[101], int v, int V)
 {
 
-    if (nodeColors.find(n) != nodeColors.end())
-        return false;
-
-    for (int i = 0; i < N; i++)
-    {
-        if (!graph[n][i])
-            continue;
-
-        if (nodeColors.find(i) == nodeColors.end())
-            continue;
-
-        if (nodeColors[i] != m)
-            return false;
-    }
-
-    return true;
-}
-
-bool graphColoring(bool graph[N][N], int m, unordered_map<int, int> &nodeColors)
-{
-    int n;
-
-    if (isGraphColored(n, nodeColors))
+    if (v == V)
         return true;
 
-    for (int i = 0; i < m; i++)
+    for (int c = 1; c <= m; c++)
     {
-        if (
-            isValid(graph, n, m, nodeColors))
+
+        if (isSafe(v, graph, color, c, V))
         {
-            nodeColors[n] = i;
-            if (graphColoring(graph, m, nodeColors))
+
+            color[v] = c;
+
+            if (graphColoringUtil(graph, m, color, v + 1, V) == true)
+
                 return true;
 
-            nodeColors.erase(n);
+            color[v] = 0;
         }
     }
 
     return false;
 }
 
+bool graphColoring(bool graph[101][101], int m, int n)
+{
+    int *color = new int[n];
+    for (int i = 0; i < n; i++)
+        color[i] = 0;
+
+    if (graphColoringUtil(graph, m, color, 0, n) == false)
+    {
+        return false;
+    }
+
+    return true;
+}
+
 int main()
 {
-    bool graph[N][N] =
-        {{0, 1, 1, 1},
-         {1, 0, 1, 0},
-         {1, 1, 0, 1},
-         {1, 0, 1, 0}};
+    bool graph[101][101] = {
+        {0, 1, 1, 1},
+        {1, 0, 1, 0},
+        {1, 1, 0, 1},
+        {1, 0, 1, 0}};
 
-    printGrid(graph);
-
+    int n = 4;
     int m = 3;
-
     unordered_map<int, int> nodeColors;
 
-    cout << (graphColoring(graph, m, nodeColors) ? "Yes" : "No");
-
-    cout << endl;
+    cout << (graphColoring(graph, m, n) ? "Yes" : "No") << endl;
 
     return 0;
 }
