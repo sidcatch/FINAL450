2b17388e7ce3e7590e7c05c3888a56e10f6b80c9
Syed Imauddin <syedprogrammer@yahoo.com>
2024-04-25 11:07:13 +0530

    The Knight’s tour

---
+++ backtracking/knights-tour.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void printBoard(vector<vector<bool>> &board)
+{
+    int n = board.size();
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << board[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+bool isValid(vector<vector<bool>> &visited, int i, int j)
+{
+    int n = visited.size();
+
+    if (i > n - 1 || j > n - 1 || i < 0 || j < 0)
+        return false;
+
+    return !visited[i][j];
+}
+
+bool knightsTour(vector<vector<bool>> &visited, int i, int j, vector<int> &path)
+{
+
+    static vector<vector<int>> displacements = {{1, 2}, {2, 1}, {-1, 2}, {2, -1}, {1, -2}, {-2, 1}, {-1, -2}, {-2, -1}};
+
+    int n = visited.size();
+
+    if (path.size() == n * n - 1)
+    {
+        for (int position : path)
+            cout << position << " ";
+        cout << endl;
+        return true;
+    }
+
+    for (vector<int> displacement : displacements)
+    {
+        int x = i + displacement[0];
+        int y = j + displacement[1];
+
+        if (isValid(visited, x, y))
+        {
+            path.push_back((y * n) + x);
+            visited[x][y] = true;
+
+            if (knightsTour(visited, x, y, path))
+                return true;
+
+            path.pop_back();
+            visited[x][y] = false;
+        }
+    }
+
+    return false;
+}
+
+int main()
+{
+    int n = 8;
+
+    vector<vector<bool>> board(n, vector<bool>(n, false));
+
+    vector<int> path = {0};
+
+    board[0][0] = true;
+
+    printBoard(board);
+
+    knightsTour(board, 0, 0, path);
+
+    return 0;
+}
