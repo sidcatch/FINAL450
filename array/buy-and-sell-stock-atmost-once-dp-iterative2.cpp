e820f34a33d5cd125c6dd1af6cc635a6c406ca34
Syed Imauddin <syedprogrammer@yahoo.com>
2024-05-13 11:15:01 +0530

    comment

--- array/buy-and-sell-stock-atmost-once-dp-iterative2.cpp
+++ array/buy-and-sell-stock-atmost-once-dp-iterative2.cpp
@@ -9,7 +9,7 @@ int maxProfit(vector<int> &prices)
 
     vector<vector<int>> dp(n, vector<int>(2));
 
-    dp[0][1] = -prices[0]; // The maximum profit on day i when holding 1 stock or can buy
+    dp[0][1] = -prices[0]; // The maximum profit on day i when holding 1 stock or can buy (which means we either bought it today or earlier)
     dp[0][0] = 0;          // The maximum profit on day i when holding 0 stocks or can not buy
 
     for (int i = 1; i < n; i++)
