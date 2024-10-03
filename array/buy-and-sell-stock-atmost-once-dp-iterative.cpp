a566eb1522456848942c1fce415ea8f33b5fbafb
Syed Imauddin <syedprogrammer@yahoo.com>
2024-10-03 12:18:14 +0530

    comments

--- array/buy-and-sell-stock-atmost-once-dp-iterative.cpp
+++ array/buy-and-sell-stock-atmost-once-dp-iterative.cpp
@@ -9,13 +9,13 @@ int maxProfit(vector<int> &prices)
 
     vector<vector<int>> dp(n, vector<int>(2));
 
-    dp[0][0] = -prices[0]; // The maximum profit on day i when holding 1 stock or not sold today or earlier
-    dp[0][1] = 0;          // The maximum profit on day i when holding 0 stocks or sold today or earlier
+    dp[0][0 /* 1 stock */] = -prices[0]; // The maximum profit on day i when holding 1 stock or not sold today or earlier
+    dp[0][1 /* 0 stocks */] = 0;         // The maximum profit on day i when holding 0 stocks or sold today or earlier
 
     for (int i = 1; i < n; i++)
     {
-        dp[i][0] = max(dp[i - 1][0], -prices[i]);
-        dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+        dp[i][0 /* 1 stock */] = max(dp[i - 1][0 /* 1 stock */], -prices[i]);
+        dp[i][1 /* 0 stocks */] = max(dp[i - 1][1], dp[i - 1][0 /* 1 stock */] + prices[i]);
     }
 
     return max(dp.back()[0], dp.back()[1]);
---
+++ array/buy-and-sell-stock-atmost-once-dp-recursive2-ACCEPTED.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int findMaximumProfit(vector<int> &prices, int i, bool canBuy, vector<vector<int>> &v)
+{
+
+    if (i < 0)
+        return 0;
+
+    if (i == 0 && canBuy)
+        return v[i][canBuy] = -prices[0];
+    if (i == 0 && !canBuy)
+        return v[i][canBuy] = 0;
+
+    if (v[i][canBuy] != -1)
+        return v[i][canBuy];
+
+    if (canBuy)
+        return v[i][canBuy] = max(-prices[i],
+                                  findMaximumProfit(prices, i - 1, canBuy, v));
+
+    else
+        return v[i][canBuy] = max(prices[i] + findMaximumProfit(
+                                                  prices, i - 1, !canBuy, v),
+                                  findMaximumProfit(prices, i - 1, canBuy, v));
+}
+
+int maxProfit(vector<int> &prices)
+{
+
+    int n = prices.size();
+    vector<vector<int>> v(n, vector<int>(2, -1));
+
+    return findMaximumProfit(prices, n - 1, false, v);
+}
+
+int main()
+{
+    vector<int> prices = {7, 1, 5, 3, 6, 4};
+
+    int ans = maxProfit(prices);
+
+    cout << ans << endl;
+
+    return 0;
+}
