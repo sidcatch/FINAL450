131d82cea3262310d76434bfbda135a12f488b26
Syed Imauddin <syedprogrammer@yahoo.com>
2024-10-03 12:58:09 +0530

    comments

--- array/buy-and-sell-stock-atmost-once-dp-iterative2-ACCEPTED.cpp
+++ array/buy-and-sell-stock-atmost-once-dp-iterative2-ACCEPTED.cpp
@@ -3,29 +3,6 @@ using namespace std;
 
 // Also see, buy-and-sell-stock-atmost-once-dp-recursive2.cpp
 
-// void printDP(vector<vector<int>> dp)
-// {
-//     for (int i = 0; i < dp.size(); i++)
-//     {
-//         // Truncate numbers if they are greater than 9999 or smaller than -999
-//         int value0 = min(max(dp[i][0], -999), 9999);
-//         int value1 = min(max(dp[i][1], -999), 9999);
-
-//         // Output each number with exactly 4 spaces
-//         cout << setw(4) << value0 << " ";
-//     }
-//     cout << endl;
-//     for (int i = 0; i < dp.size(); i++)
-//     {
-//         // Truncate numbers if they are greater than 9999 or smaller than -999
-//         int value1 = min(max(dp[i][1], -999), 9999);
-
-//         // Output each number with exactly 4 spaces
-//         cout << setw(4) << value1 << " ";
-//     }
-//     cout << endl;
-// }
-
 int maxProfit(vector<int> &prices)
 {
     int n = prices.size();
@@ -37,9 +14,12 @@ int maxProfit(vector<int> &prices)
 
     for (int i = 1; i < n; i++)
     {
-        dp[i][1] = max(dp[i - 1][1], -prices[i]);
+        dp[i][1] = max(dp[i - 1][1], -prices[i]); // Locks to minimum price.
         dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
     }
+
+    printVector(prices);
+    printDP(dp);
 
     return max(dp.back()[0], dp.back()[1]);
     // return dp.back()[0];
@@ -47,11 +27,47 @@ int maxProfit(vector<int> &prices)
 
 int main()
 {
-
     vector<int> prices = {7, 1, 5, 3, 6, 4};
 
     int ans = maxProfit(prices);
 
     cout << ans << endl;
     return 0;
+}
+
+void printDP(vector<vector<int>> &dp)
+{
+    for (int i = 0; i < dp.size(); i++)
+    {
+        // Truncate numbers if they are greater than 9999 or smaller than -999
+        int value0 = min(max(dp[i][0], -999), 9999);
+        int value1 = min(max(dp[i][1], -999), 9999);
+
+        // Output each number with exactly 4 spaces
+        cout << setw(4) << value0 << " ";
+    }
+    cout << endl;
+    for (int i = 0; i < dp.size(); i++)
+    {
+        // Truncate numbers if they are greater than 9999 or smaller than -999
+        int value1 = min(max(dp[i][1], -999), 9999);
+
+        // Output each number with exactly 4 spaces
+        cout << setw(4) << value1 << " ";
+    }
+    cout << endl;
+}
+
+void printVector(vector<int> &v)
+{
+    for (int i = 0; i < v.size(); i++)
+    {
+        // Truncate numbers if they are greater than 9999 or smaller than -999
+        int value0 = min(max(v[i], -999), 9999);
+        int value1 = min(max(v[i], -999), 9999);
+
+        // Output each number with exactly 4 spaces
+        cout << setw(4) << value0 << " ";
+    }
+    cout << endl;
 }
