867c735f09e6533bf3c7eaa4bb318f7648783b63
Syed Imauddin <syedprogrammer@yahoo.com>
2024-10-08 11:16:15 +0530

    tweak

--- array/buy-and-sell-stock-infinite-with-cool-down-dp-recursive.cpp
+++ array/buy-and-sell-stock-infinite-with-cool-down-dp-recursive.cpp
@@ -4,22 +4,22 @@ using namespace std;
 
 //See gfg for more methods of solving!
 
-int findMaximumProfit(vector<int> &prices, int i, bool buy, vector<vector<int>> &v)
+int findMaximumProfit(vector<int> &prices, int i, bool holding, vector<vector<int>> &v)
 {
 
     if (i >= prices.size())
         return 0;
 
-    if (v[i][buy] != -1)
-        return v[i][buy];
+    if (v[i][holding] != -1)
+        return v[i][holding];
 
-    if (buy)
-        return v[i][buy] = max(-prices[i] + findMaximumProfit(prices, i + 1, !buy, v),
-                               findMaximumProfit(prices, i + 1, buy, v));
+    if (holding)
+        return v[i][holding] = max(-prices[i] + findMaximumProfit(prices, i + 1, !holding, v),
+                                   findMaximumProfit(prices, i + 1, holding, v));
 
     else
-        return v[i][buy] = max(prices[i] + findMaximumProfit(prices, i + 2, !buy, v), //Skip one day as a cool because of cool down period!
-                               findMaximumProfit(prices, i + 1, buy, v));
+        return v[i][holding] = max(prices[i] + findMaximumProfit(prices, i + 2, !holding, v), //Skip one day as a cool because of cool down period!
+                                   findMaximumProfit(prices, i + 1, holding, v));
 }
 
 int maxProfit(vector<int> &prices)
--- array/buy-and-sell-stock-infinite-with-cool-down-dp-recursive2-WRONG.cpp
+++ array/buy-and-sell-stock-infinite-with-cool-down-dp-recursive2-WRONG.cpp
@@ -2,20 +2,25 @@
 #include <iostream>
 using namespace std;
 
-int findMaximumProfit(vector<int> &prices, int i, bool buy, vector<vector<int>> &v)
+int findMaximumProfit(vector<int> &prices, int i, bool holding, vector<vector<int>> &v)
 {
     if (i < 0)
         return 0;
 
-    if (v[i][buy] != -1)
-        return v[i][buy];
+    if (i == 0 && holding)
+        return v[i][holding] = -prices[0];
+    if (i == 0 && !holding)
+        return v[i][holding] = 0;
 
-    if (buy)
-        return v[i][buy] = max(-prices[i] + findMaximumProfit(prices, i - 1, !buy, v),
-                               findMaximumProfit(prices, i - 1, buy, v));
+    if (v[i][holding] != -1)
+        return v[i][holding];
+
+    if (holding)
+        return v[i][holding] = max(prices[i] + findMaximumProfit(prices, i - 2, !holding, v),
+                                   findMaximumProfit(prices, i - 1, holding, v));
     else
-        return v[i][buy] = max(prices[i] + findMaximumProfit(prices, i - 2, !buy, v), // Skip one day as cooldown
-                               findMaximumProfit(prices, i - 1, buy, v));
+        return v[i][holding] = max(-prices[i] + findMaximumProfit(prices, i - 1, !holding, v),
+                                   findMaximumProfit(prices, i - 1, holding, v));
 }
 
 int maxProfit(vector<int> &prices)
@@ -23,12 +28,12 @@ int maxProfit(vector<int> &prices)
     int n = prices.size();
     vector<vector<int>> v(n, vector<int>(2, -1));
 
-    return findMaximumProfit(prices, n - 1, 1, v);
+    return findMaximumProfit(prices, n - 1, 0, v);
 }
 
 int main()
 {
-    vector<int> prices = {1, 2, 3, 0, 2};
+    vector<int> prices = {1, 2};
 
     int ans = maxProfit(prices);
 
